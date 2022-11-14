2d04614dbc1cd5ddd4b640fc749b88ba5bfc6d96
paulogonzalez5679 <paulomga10@gmail.com>
2022-11-13 20:29:57 -0500

    Navegacion funcionando

---
+++ ProgramacionMovil/src/Components/pagina2/pagina2.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Pagina2Component } from './pagina2.component';
+
+
+
+@NgModule({
+  declarations: [
+    Pagina2Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Pagina2Module { }
---
+++ ProgramacionMovil/src/Components/pagina3/pagina3.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Pagina3Component } from './pagina3.component';
+
+
+
+@NgModule({
+  declarations: [
+    Pagina3Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Pagina3Module { }
