164709f98de32404a16d30369af3349603bc84b8
paulogonzalez5679 <paulomga10@gmail.com>
2022-11-21 08:57:40 -0500

    actulización

--- ProgramacionMovil/src/Components/home/home.component.ts
+++ ProgramacionMovil/src/Components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,10 +11,16 @@ export class HomeComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+
   }
 
-  navegacion(){
+  navegar(){
     this.router.navigate(['web2'])
+  }
+
+
+  navegacion(){
+    this.router.navigate([''])
   }
 
   navegacion1()
--- ProgramacionMovil/src/app/app-routing.module.ts
+++ ProgramacionMovil/src/app/app-routing.module.ts
@@ -1,20 +1,37 @@
-import { Pagina2Component } from './../Components/pagina2/pagina2.component';
 import { HomeComponent } from './../Components/home/home.component';
+import { Pagina2Component } from './../Components/pagina2/pagina2.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Pagina3Component } from 'src/Components/pagina3/pagina3.component';
 
+
+
 const routes: Routes = [
   {
-    path: '', component: HomeComponent
+    path:'', component: HomeComponent
   },
   {
-    path:'pagina2', component: Pagina2Component
-  },
-  {
-    path:'pagina3', component: Pagina3Component
+    path:'web2', component: Pagina2Component
   }
 ];
+
+
+  // {
+  //   path: '', component: HomeComponent
+  // },
+  // {
+  //   path:'web2', component: Pagina2Component
+  // }
+
+  // {
+  //   path: '', component: HomeComponent
+  // },
+  // {
+  //   path:'pagina2', component: Pagina2Component
+  // },
+  // {
+  //   path:'pagina3', component: Pagina3Component
+  // }
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
